9839c1faaaf3f15ce4a1852ea782646551fa24b3
Jajangja2412 <jajang.jpr@gmail.com>
2024-06-04 11:54:54 +0700

    update id_grn di tabel detail grn detail

--- resources/views/grn/action_generate_lot.blade.php
+++ resources/views/grn/action_generate_lot.blade.php
@@ -1,11 +1,11 @@
 @if($data->lot_number == '')
     <button type="button" class="btn btn-success btn-sm waves-effect waves-light"
-    data-bs-toggle="modal" onclick="lot_number('{{ $data->id }}');"
+    data-bs-toggle="modal" onclick="lot_number('{{ $data->id }}','{{ $data->id_good_receipt_notes }}');"
     data-bs-target="#myModal"><i class="bx bx-edit-alt" title="Input Lot"></i> Input Lot</button>
     <!-- Include modal content -->
 @elseif($data->lot_number != '')
 <button type="button" class="btn btn-success btn-sm waves-effect waves-light"
-    data-bs-toggle="modal" onclick="lot_number_edit('{{ $data->id }}','{{ $data->lot_number }}');"
+    data-bs-toggle="modal" onclick="lot_number_edit('{{ $data->id }}','{{ $data->lot_number }}','{{ $data->id_good_receipt_notes }}');"
     data-bs-target="#input_lot_edit"><i class="bx bx-edit-alt" title="Input Lot"></i> Input Lot</button>
 @endif
 
